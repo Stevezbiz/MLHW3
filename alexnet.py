fa2864a6f102da7e26275b2dc2afe6000fc91c4a
Stevezbiz <61883836+Stevezbiz@users.noreply.github.com>
2020-12-12 16:09:36 +0100

    Add files via upload

--- alexnet.py
+++ alexnet.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 # from .utils import load_state_dict_from_url
 from typing import Any
+import torch.nn as nn
+from torch.autograd import Function
 
 try:
     from torch.hub import load_state_dict_from_url
@@ -15,10 +17,24 @@ model_urls = {
     'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
 }
 
+class ReverseLayerF(Function):
+    # Forwards identity
+    # Sends backward reversed gradients
+    @staticmethod
+    def forward(ctx, x, alpha):
+        ctx.alpha = alpha
+
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        output = grad_output.neg() * ctx.alpha
+
+        return output, None
 
 class AlexNet(nn.Module):
 
-    def __init__(self, num_classes: int = 1000, num_domains: int = 2) -> None:
+    def __init__(self, num_classes: int = 1000) -> None:
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
@@ -52,7 +68,7 @@ class AlexNet(nn.Module):
             nn.Dropout(),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
-            nn.Linear(4096, num_domains),
+            nn.Linear(4096, num_classes),
         )
 
     def forward(self, x: torch.Tensor, alpha=None) -> torch.Tensor:
